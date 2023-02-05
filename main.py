3a4680f2612c33c7e410f63d9bce55230683cd2d
biazinha18 <biaamada17@gmail.com>
2023-02-05 20:39:20 -0300

    Projeto aluguel carros versao lista

--- main.py
+++ main.py
@@ -1,74 +1,97 @@
 from flask import Flask, jsonify, request, render_template
 
+# Antiga DB -> 
+carros = []
+
 app = Flask(__name__)
 
-contacts = [{'id': 1, 'name': 'John Doe',  'phone': '555-555-5555'}]
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+# http://127.0.0.1:5000
+# Rotas ---------------------------------------------------------------------------------------------------------------
+@app.route('/', methods=['GET'])
+def main():
+    return render_template('main.html')
 
-# GET request to retrieve all contacts
-@app.route('/contacts', methods=['GET'])
-def get_contacts():
-    if not contacts:
-        return jsonify({'message':'No contacts founded in server'}), 404
-    return jsonify({'contacts': contacts}), 200
+@app.route('/cadastro', methods=['GET'])
+def cadastro():
+    return render_template('cadastro.html')
 
-# GET request to retrieve one contacts
-@app.route('/contacts/<int:id>', methods=['GET'])
-def get_contact(id):
-    for contact in contacts:
-        if id == contact['id']:
-            return jsonify({'contact': contact}),200
-    return jsonify({'message':'contact not found'}), 404
+@app.route('/update/<string:modelo>/<string:ano>', methods=['GET'])
+def update(modelo, ano):
+    for carro in carros:
+        if modelo == carro['modelo'] and ano == carro['ano']:
+            print(carro['status'])
+            return render_template('update.html', data=carro)        
+    return render_template('update.html', data={})
 
-# POST request to add a new contact with data of the new contact on a json file
-@app.route('/contacts', methods=['POST'])
-def add_contact():
+# GET request to retrieve all carros
+@app.route('/carros', methods=['GET'])
+def get_carros():
+    if not carros:
+        return jsonify({'message':'No cars founded in server'}), 404
+    return jsonify({'carros': carros}), 200
+
+# GET request to retrieve one car
+@app.route('/carros/<string:modelo>/<string:ano>', methods=['GET'])
+def get_carro(modelo, ano):
+    for carro in carros:
+        if modelo == carro['modelo'] and ano == carro['ano']:
+            return render_template('car.html', data=carro)
+    return jsonify({'message':'carro not found'}), 404
+
+# POST request to add a new carro with data of the new carro on a json file
+@app.route('/carros', methods=['POST'])
+def add_carro():
     if not request.is_json:
         return jsonify({'message':'body is not a json'}), 415
     
     data = request.get_json()
 
-    if not data or not all(key in data for key in ('name','phone')):
+    if not data or not all(key in data for key in ('modelo','marca', 'ano', 'observacoes', 'valor', 'status')):
         return jsonify({'message':'bad request'}), 400
     
     id = 1
     
-    if len(contacts) > 0:
-        id = contacts[-1]['id'] + 1
-    c = {'id':id,'name':data['name'],'phone':data['phone']}
+    if len(carros) > 0:
+        id = carros[-1]['id'] + 1
 
-    contacts.append(c) 
+    c = {'id':id, 'image':'https://autoentusiastas.com.br/ae/wp-content/uploads/2016/01/Novo-Jetta-Highline-9-1140x720.jpg'}
 
-    return jsonify({'contact': c}), 201
+    c.update(data)
 
-# PUT request to update a contact
-@app.route('/contacts/<int:id>', methods=['PUT'])
-def update_contact(id):
+    carros.append(c) 
+
+    return jsonify({'carro': c}), 201
+
+# PUT request to update a carro
+@app.route('/carros/<int:id>', methods=['PUT'])
+def update_carro(id):
     if not request.is_json:
         return jsonify({'message':'body is not a json'}), 415
 
     data = request.get_json()
 
-    if not data or not all(key in data for key in ('name','phone')):
+    if not data or not all(key in data for key in ('modelo','marca', 'ano', 'observacoes', 'valor', 'status')):
         return jsonify({'message':'bad request'}), 400
 
-    for i,contact in enumerate(contacts):
-        if contact['id'] == id:
-            contacts[i] = {'id':id, 'name':data['name'],'phone':data['phone']}    
-            return jsonify({'contact': contacts[i]}),200
+    for i,carro in enumerate(carros):
+        if carro['id'] == id:
+            c = {'id':id, 'image':'https://autoentusiastas.com.br/ae/wp-content/uploads/2016/01/Novo-Jetta-Highline-9-1140x720.jpg'}
+            c.update(data)
+            carros[i] = c
+            return jsonify({'carro': carros[i]}),200
             
-    return jsonify({'message':'contact not found'}), 404
+    return jsonify({'message':'carro not found'}), 404
 
-# DELETE request to delete a contact
-@app.route('/contacts/<int:id>', methods=['DELETE'])
-def delete_contact(id):
-    for i,contact in enumerate(contacts):
-        if contact['id'] == id:
-            del contacts[i]   
-            return jsonify({'message': 'contact deleted'}),200
-    return jsonify({'message':'contact not found'}), 404
+# DELETE request to delete a carro
+@app.route('/carros/<int:id>', methods=['DELETE'])
+def delete_carro(id):
+    for i,carro in enumerate(carros):
+        if carro['id'] == id:
+            del carros[i]   
+            return jsonify({'message': 'carro deleted'}),200
+    return jsonify({'message':'carro not found'}), 404
+
+# ---------------------------------------------------------------------------------------------------------
 
 app.run(debug=True)
