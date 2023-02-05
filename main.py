2662df201b00e2f2ff1ad63f4a32eab54e6b9f0c
biazinha18 <biaamada17@gmail.com>
2023-02-05 20:44:44 -0300

    Revert "Projeto aluguel carros versao lista"
    This reverts commit 3a4680f2612c33c7e410f63d9bce55230683cd2d.

--- main.py
+++ main.py
@@ -1,97 +1,74 @@
 from flask import Flask, jsonify, request, render_template
 
-# Antiga DB -> 
-carros = []
-
 app = Flask(__name__)
 
+contacts = [{'id': 1, 'name': 'John Doe',  'phone': '555-555-5555'}]
 
-# http://127.0.0.1:5000
-# Rotas ---------------------------------------------------------------------------------------------------------------
-@app.route('/', methods=['GET'])
-def main():
-    return render_template('main.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
-@app.route('/cadastro', methods=['GET'])
-def cadastro():
-    return render_template('cadastro.html')
+# GET request to retrieve all contacts
+@app.route('/contacts', methods=['GET'])
+def get_contacts():
+    if not contacts:
+        return jsonify({'message':'No contacts founded in server'}), 404
+    return jsonify({'contacts': contacts}), 200
 
-@app.route('/update/<string:modelo>/<string:ano>', methods=['GET'])
-def update(modelo, ano):
-    for carro in carros:
-        if modelo == carro['modelo'] and ano == carro['ano']:
-            print(carro['status'])
-            return render_template('update.html', data=carro)        
-    return render_template('update.html', data={})
+# GET request to retrieve one contacts
+@app.route('/contacts/<int:id>', methods=['GET'])
+def get_contact(id):
+    for contact in contacts:
+        if id == contact['id']:
+            return jsonify({'contact': contact}),200
+    return jsonify({'message':'contact not found'}), 404
 
-# GET request to retrieve all carros
-@app.route('/carros', methods=['GET'])
-def get_carros():
-    if not carros:
-        return jsonify({'message':'No cars founded in server'}), 404
-    return jsonify({'carros': carros}), 200
-
-# GET request to retrieve one car
-@app.route('/carros/<string:modelo>/<string:ano>', methods=['GET'])
-def get_carro(modelo, ano):
-    for carro in carros:
-        if modelo == carro['modelo'] and ano == carro['ano']:
-            return render_template('car.html', data=carro)
-    return jsonify({'message':'carro not found'}), 404
-
-# POST request to add a new carro with data of the new carro on a json file
-@app.route('/carros', methods=['POST'])
-def add_carro():
+# POST request to add a new contact with data of the new contact on a json file
+@app.route('/contacts', methods=['POST'])
+def add_contact():
     if not request.is_json:
         return jsonify({'message':'body is not a json'}), 415
     
     data = request.get_json()
 
-    if not data or not all(key in data for key in ('modelo','marca', 'ano', 'observacoes', 'valor', 'status')):
+    if not data or not all(key in data for key in ('name','phone')):
         return jsonify({'message':'bad request'}), 400
     
     id = 1
     
-    if len(carros) > 0:
-        id = carros[-1]['id'] + 1
+    if len(contacts) > 0:
+        id = contacts[-1]['id'] + 1
+    c = {'id':id,'name':data['name'],'phone':data['phone']}
 
-    c = {'id':id, 'image':'https://autoentusiastas.com.br/ae/wp-content/uploads/2016/01/Novo-Jetta-Highline-9-1140x720.jpg'}
+    contacts.append(c) 
 
-    c.update(data)
+    return jsonify({'contact': c}), 201
 
-    carros.append(c) 
-
-    return jsonify({'carro': c}), 201
-
-# PUT request to update a carro
-@app.route('/carros/<int:id>', methods=['PUT'])
-def update_carro(id):
+# PUT request to update a contact
+@app.route('/contacts/<int:id>', methods=['PUT'])
+def update_contact(id):
     if not request.is_json:
         return jsonify({'message':'body is not a json'}), 415
 
     data = request.get_json()
 
-    if not data or not all(key in data for key in ('modelo','marca', 'ano', 'observacoes', 'valor', 'status')):
+    if not data or not all(key in data for key in ('name','phone')):
         return jsonify({'message':'bad request'}), 400
 
-    for i,carro in enumerate(carros):
-        if carro['id'] == id:
-            c = {'id':id, 'image':'https://autoentusiastas.com.br/ae/wp-content/uploads/2016/01/Novo-Jetta-Highline-9-1140x720.jpg'}
-            c.update(data)
-            carros[i] = c
-            return jsonify({'carro': carros[i]}),200
+    for i,contact in enumerate(contacts):
+        if contact['id'] == id:
+            contacts[i] = {'id':id, 'name':data['name'],'phone':data['phone']}    
+            return jsonify({'contact': contacts[i]}),200
             
-    return jsonify({'message':'carro not found'}), 404
+    return jsonify({'message':'contact not found'}), 404
 
-# DELETE request to delete a carro
-@app.route('/carros/<int:id>', methods=['DELETE'])
-def delete_carro(id):
-    for i,carro in enumerate(carros):
-        if carro['id'] == id:
-            del carros[i]   
-            return jsonify({'message': 'carro deleted'}),200
-    return jsonify({'message':'carro not found'}), 404
-
-# ---------------------------------------------------------------------------------------------------------
+# DELETE request to delete a contact
+@app.route('/contacts/<int:id>', methods=['DELETE'])
+def delete_contact(id):
+    for i,contact in enumerate(contacts):
+        if contact['id'] == id:
+            del contacts[i]   
+            return jsonify({'message': 'contact deleted'}),200
+    return jsonify({'message':'contact not found'}), 404
 
 app.run(debug=True)
---
+++ static/contact.js
@@ -0,0 +1,50 @@
+function send_json_request(method, formid, variable){
+    var request_url = '/contacts'
+    var data = $('#'+formid).serializeArray().reduce(function(obj, item) {
+        obj[item.name] = item.value;
+        return obj;
+    }, {});
+    if(method == 'PUT' || method == 'DELETE')
+        request_url = request_url + "/" + data[variable]
+
+    var list = JSON.stringify(data);
+    $.ajax({
+        type: method,
+        url: request_url,
+        data: list,
+        dataType: "json",
+        contentType: "application/json"
+    });
+}
+
+function find_contact(event){
+    event.preventDefault();
+    const id = document.getElementById('find_id').value;
+    $.get("/contacts/"+id, function(data, status){
+        let placeholder = document.querySelector('#find_data');
+        let contact = data['contact']
+        let out = `
+            <tr>
+                <td>${contact.id}</td>
+                <td>${contact.name}</td>
+                <td>${contact.phone}</td>
+            </tr>
+        `
+        placeholder.innerHTML = out;
+    });
+}
+
+$.get("/contacts", function(data, status){
+    let placeholder = document.querySelector('#contacts');
+    let out = "";
+    for(let contact of data['contacts']){
+        out += `
+            <tr>
+                <td>${contact.id}</td>
+                <td>${contact.name}</td>
+                <td>${contact.phone}</td>
+            </tr>
+        `
+    }
+    placeholder.innerHTML = out;
+});
